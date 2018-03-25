4025332123d0aea7031243e6fc630493386dab0a
joncoe <info@joncoe.com>
2018-03-24 20:49:39 -0400

    Add form fields and state for the Add Event view

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import ListEvents from './components/ListEvents';
+import AddEvent from './components/AddEvent';
 
 class App extends Component {
   render() {
     return (
-      // <Router>
-      //   <div>
-      //     <Route exact path="/" component={Home} />
-      //     <Route exact path="/events" component={ListEvents} />
-      //   </div>
-      // </Router>
-      <ListEvents />
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/events" component={ListEvents} />
+          <Route exact path="/add-event" component={AddEvent} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/AddEvent.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+class AddEvent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      eventCity: '',
+      eventCountry: '',
+      eventVenue: '',
+      performanceDate: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add an Event</h1>
+        <div className="form">
+          <div className="input-roup">
+            <label>City</label>
+            <input
+              type="text"
+              name="eventCity"
+              placeholder="City"
+              value={this.state.eventCity}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+
+          <div className="input-roup">
+            <label>Country</label>
+            <input
+              type="text"
+              name="eventCountry"
+              placeholder="Country"
+              {this.state.eventCountry}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+
+          <div className="input-roup">
+            <label>Venue</label>
+            <input
+              type="text"
+              name="eventVenue"
+              placeholder="Venue"
+              {this.state.eventVenue}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+
+          <div className="input-roup">
+            <label>Event Date</label>
+            <input
+              type="date"
+              name="performanceDate"
+              placeholder="Performance Date"
+              {this.state.performanceDate}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+          <button onClick={this.addEvent}>Add New Event</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddEvent;
